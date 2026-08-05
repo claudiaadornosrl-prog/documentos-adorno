d918f1936ad621b69762e20122165b635e6dcc79
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-08-05 13:01:06 -0300

    deploy 2026-08-05 13:01

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Estrategia: network-first (siempre intentar última versión)
 // ═══════════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'docs-v17-observar-facturas';
+const CACHE_VERSION = 'docs-v18-observar-junto-control';
 const CACHE_ASSETS = [
   'fonts/URWGothic-Book.ttf',
   './',
